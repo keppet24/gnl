4a51307aaab6d7552247f8fdb0da79aa89022efc
Othmane Ettaqi <othmaneettaqi@MacBook-Air-de-Othmane.local>
2024-12-01 23:58:16 +0100

    lessgo

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oettaqi <oettaqi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: othmaneettaqi <othmaneettaqi@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/29 12:50:23 by oettaqi           #+#    #+#             */
-/*   Updated: 2024/11/30 18:55:01 by oettaqi          ###   ########.fr       */
+/*   Created: 2024/12/01 17:43:18 by othmaneetta       #+#    #+#             */
+/*   Updated: 2024/12/01 23:55:24 by othmaneetta      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,120 +49,97 @@ char	*ft_substr(char *s, int start, size_t len)
 	return (resu);
 }
 
-// char *read_one_line(int fd)
+// char	*get_next_line(int fd)
 // {
-//     static char *reste = NULL; // Contient le reste à traiter
-//     char *buf;
-//     char *temp;
-//     char *resu;
-//     int nb_read;
+// 	static char	*stash;
+// 	char	*temp;
+// 	char	*buf;
+// 	char	*line;
+// 	int		nb_read;
 
-//     if (!reste)
-//         reste = ft_strdup(""); // Initialisation si nécessaire
-    
-//     buf = malloc(sizeof(char) * (BUFFER_SIZE + 1)); // +1 pour '\0'
-//     if (!buf)
-//         return NULL;
+// 	temp = ft_strdup("");
+// 	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+// 	if (!buf)
+// 	{
+// 		free(buf);
+// 		return (NULL);
+// 	}
+// 	nb_read = 1;
+// 	while (nb_read > 0)
+// 	{
+// 		nb_read = read(fd, buf, BUFFER_SIZE);
+// 		buf[nb_read] = 0;
+// 		stash = ft_strjoin(temp, buf);
+// 		free(temp);
+// 		temp = stash;
+// 		if (ft_strchr(stash, '\n'))
+// 		{
+// 			line = ft_substr(stash, 0, ft_strchr(stash, '\n') - stash + 1);
+// 			stash = ft_strdup(&stash[ft_strchr(stash, '\n') - stash + 1 ]);
+// 			temp = stash;
+// 			break;
+// 		}
+// 	}
+// 	if (nb_read == 0)
+// 		return (stash);
+// 	free(buf);
+// 	return (line);
+// } 
 
-//     nb_read = 1; // Initialiser à une valeur non nulle pour entrer dans la boucle
-//     while (nb_read > 0)
-//     {
-//         nb_read = read(fd, buf, BUFFER_SIZE);
-//         if (nb_read < 0)
-//             break;
-//         buf[nb_read] = '\0'; // Terminer la chaîne lue
-//         temp = ft_strjoin(reste, buf); // Ajouter le buffer à `reste`
-//         free(reste); // Libérer l'ancien `reste`
-//         reste = temp;
-
-//         if (ft_strchr(reste, '\n')) // Une ligne complète est trouvée
-//         {
-//             resu = ft_substr(reste, 0, ft_strchr(reste, '\n') - reste); // Extraire la ligne
-//             temp = ft_strdup(ft_strchr(reste, '\n') + 1); // Récupérer le nouveau reste
-//             free(reste); // Libérer l'ancien `reste`
-//             reste = temp;
-//             free(buf); // Libérer le buffer
-//             return resu; // Retourner la ligne extraite
-//         }
-//     }
-
-//     // Gérer la fin de fichier ou reste final
-//     free(buf); // Libérer le buffer
-//     if (*reste) // Si `reste` contient encore des données
-//     {
-//         resu = ft_strdup(reste);
-//         free(reste);
-//         reste = NULL;
-//         return resu;
-//     }
-//     free(reste);
-//     reste = NULL;
-//     return NULL; // Fin du fichier ou erreur
-// }
-
-
-char	*read_one_line(int fd)
+char	*get_next_line(int fd)
 {
-	static char		*reste;
-	int			nb_read;
-	char		*buf;
-	char		*temp;
-	char		*resu;
+	static char	*stash;
+	char	*temp;
+	char	*buf;
+	char	*line;
+	int		nb_read;
 
-	buf = malloc(sizeof(char) * (BUFFER_SIZE));
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!stash)
+		stash = ft_strdup("");
+	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buf)
-	{
-		free(buf);
-		return (0);
-	}
-
-	nb_read = -1;
-	if (!reste)
-		reste = ft_strdup("");
-	resu = ft_strdup("");
-	while (nb_read != 0)
+		return (NULL);
+	nb_read = 1;
+	while (nb_read > 0)
 	{
 		nb_read = read(fd, buf, BUFFER_SIZE);
-		// printf("%s", buf);
 		if (nb_read < 0)
-            break	;
-		temp = ft_strjoin(reste, buf);
-		reste = temp;
-		if (ft_strchr(reste, '\n'))
-		{
-			reste[nb_read] = '\0';
-			resu = ft_substr(reste, 0, ft_strchr(reste, '\n') - reste);
-			reste = ft_strdup(&reste[ft_strchr(reste, '\n') - reste + 1]);
-			// printf("%s\n", resu);
-			// printf("%s\n", reste);
-			reste = temp;
 			break;
+		buf[nb_read] = '\0';
+		temp = stash;
+		stash = ft_strjoin(temp, buf);
+		free(temp);
+		if (ft_strchr(stash, '\n'))
+		{
+			line = ft_substr(stash, 0, ft_strchr(stash, '\n') - stash + 1);
+			temp = stash;
+			stash = ft_strdup(&stash[ft_strchr(stash, '\n') - stash + 1]);
+			free(temp);
+			free(buf);
+			return (line);
 		}
 	}
-	if (*reste)
-    {
-        resu = ft_strdup(reste);
-        free(reste);
-        reste = NULL;
-        return resu;
-    }
-	return (resu);
+	free(buf);
+	if (stash && *stash)
+	{
+		line = stash;
+		stash = NULL;
+		free(stash);
+		return (line);
+	}
+	free(stash);
+	return (NULL);
 }
+
 
 int	main()
 {
 	int fd;
 
 	fd = open("test.txt",  O_RDONLY, O_CREAT);
-	// read_one_line(fd);
-	// printf("%c",'\n');
-	// read_one_line(fd);
-	// read_one_line(fd);
-
-	printf("%s\n",read_one_line(fd));
-	printf("%s\n",read_one_line(fd));
-	printf("%s\n",read_one_line(fd));
-	//printf("%s\n",read_one_line(fd));
-
+	printf("%s",get_next_line(fd));
+	printf("%s",get_next_line(fd));
 	return(0);
 }
